0d00aab1c4417f2bc0a0ea392217b1674c37fc3b
PSL - Bhupal Pamuluri <v-bpamuluri@topcon.com>
2026-05-12 11:54:52 +0000

    updating lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     
     response = {
         "statusCode": 200,
-        "body": "Hello from Lambda! this is bhupalkrishnareddy"
+        "body": "Hello from Lambda! this is bhupal"
     }
     
     return response
