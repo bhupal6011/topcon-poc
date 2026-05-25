341125b6113b3b639bd276d35e1872bb09f52492
bhupal6011 <pamuluri.bkreddy@gmail.com>
2026-05-25 17:12:49 +0530

    deploying dev lambda

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "my-aw-sdemo"
-    key            = var.terraform_state_file_key 
+    #key            = var.terraform_state_file_key 
     region         = "us-east-1"
     #dynamodb_table = "terraform-lock-table2"    # Replace with your DynamoDB table name
     #encrypt        = true                      # Enable server-side encryption for state file
